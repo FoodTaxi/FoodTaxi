496a5d477530789c8fd9854e1345212a9a61b3f2
m1sh0 <m.n.karev@gmail.com>
2016-12-27 20:50:44 +0200

    Initial commit

---
+++ source/backend/FoodTaxi/src/main/java/com/aim/foodtaxi/FoodTaxiApplication.java
@@ -0,0 +1,12 @@
+package com.aim.foodtaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodTaxiApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(FoodTaxiApplication.class, args);
+	}
+}
---
+++ source/backend/FoodTaxi/src/main/java/com/aim/foodtaxi/services/OrderServices.java
@@ -0,0 +1,15 @@
+package com.aim.foodtaxi.services;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+public class OrderServices {
+	
+	@RequestMapping("/home")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+}
