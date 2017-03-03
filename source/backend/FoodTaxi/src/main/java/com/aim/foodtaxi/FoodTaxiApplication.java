d88cd51ee97c9f644984f92cf35a493b5c130e69
m1sh0 <m.n.karev@gmail.com>
2017-03-03 16:16:52 +0200

    fix database connection

--- source/backend/FoodTaxi/src/main/java/com/aim/foodtaxi/FoodTaxiApplication.java
+++ source/backend/FoodTaxi/src/main/java/com/aim/foodtaxi/FoodTaxiApplication.java
@@ -16,7 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 @EnableJpaRepositories
-class FoodTaxiApplication {
+public class FoodTaxiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FoodTaxiApplication.class, args);
