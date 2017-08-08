64b4782fddd7b46eeba1f2bc1722cfe02f25af39
5koff <dim.petkoff@gmail.com>
2017-08-08 22:51:09 +0300

    partial implementation

--- source/backend/FoodTaxi/src/main/java/com/aim/foodtaxi/services/BidService.java
+++ source/backend/FoodTaxi/src/main/java/com/aim/foodtaxi/services/BidService.java
@@ -2,53 +2,50 @@ package com.aim.foodtaxi.services;
 
 import java.util.Optional;
 
-import javax.inject.Inject;
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.aim.foodtaxi.domain.BidEntity;
 import com.aim.foodtaxi.domain.DriverEntity;
 import com.aim.foodtaxi.domain.OrderEntity;
 import com.aim.foodtaxi.dto.Bid;
-import com.aim.foodtaxi.mappers.BidMapper;
 import com.aim.foodtaxi.repositories.BidRepository;
-import com.aim.foodtaxi.repositories.DriverRepository;
 import com.aim.foodtaxi.repositories.OrderRepository;
 
 @Service
 @Transactional
 public class BidService {
-//    @Inject
-//    private BidRepository bidRepository;
-//    
-//    @Inject
-//    private OrderRepository orderRepository;
-//
+    @Autowired
+    private BidRepository bidRepository;
+    
+    @Autowired
+    private DeliveryRepository orderRepository;
+
 //    @Inject
 //    private DriverRepository driverRepository;
 //    
 //    @Inject
 //    private BidMapper bidMapper;
 //
-//    public boolean createBid(Bid bid, Long orderId, Long driverId) {
-//        BidEntity bidEntity = bidMapper.bidToBidEntity(bid);
-//        Optional<OrderEntity> orderEntity = orderRepository.findOneById(orderId);
-//        Optional<DriverEntity> driverEntity = driverRepository.findOneById(driverId);
-//        if (!driverEntity.isPresent() || !orderEntity.isPresent()) {
-//            return false;
-//        }
-//        bidEntity.setDriver(driverEntity.get());
-//        bidEntity.setOrder(orderEntity.get());
-//
-//
-//        BidEntity savedEntity = bidRepository.save(bidEntity);
-//        if (orderEntity.get().getBestBid() == null || 
-//                orderEntity.get().getBestBid().getPrice() > bidEntity.getPrice()) {
-//            orderEntity.get().setBestBid(savedEntity);
-//            orderRepository.save(orderEntity.get());
-//        }
-//        return true;
-//    }
+    public boolean createBid(Bid bid, Long orderId, String driverUsername) {
+        BidEntity bidEntity = bidMapper.bidToBidEntity(bid);
+        Optional<OrderEntity> orderEntity = orderRepository.findOneById(orderId);
+        Optional<DriverEntity> driverEntity = driverRepository.findOneById(driverId);
+        if (!driverEntity.isPresent() || !orderEntity.isPresent()) {
+            return false;
+        }
+        bidEntity.setDriver(driverEntity.get());
+        bidEntity.setOrder(orderEntity.get());
 
+
+        BidEntity savedEntity = bidRepository.save(bidEntity);
+        if (orderEntity.get().getBestBid() == null || 
+                orderEntity.get().getBestBid().getPrice() > bidEntity.getPrice()) {
+            orderEntity.get().setBestBid(savedEntity);
+            orderRepository.save(orderEntity.get());
+        }
+        return true;
+    }
 }
