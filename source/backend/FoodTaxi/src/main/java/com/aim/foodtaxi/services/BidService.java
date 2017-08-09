120f578c9898c771f9060a9052f97da2ffff64cd
5koff <dim.petkoff@gmail.com>
2017-08-09 22:16:22 +0300

    implemented createBid

--- source/backend/FoodTaxi/src/main/java/com/aim/foodtaxi/services/BidService.java
+++ source/backend/FoodTaxi/src/main/java/com/aim/foodtaxi/services/BidService.java
@@ -8,43 +8,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.aim.foodtaxi.domain.BidEntity;
+import com.aim.foodtaxi.domain.DeliveryEntity;
 import com.aim.foodtaxi.domain.DriverEntity;
-import com.aim.foodtaxi.domain.OrderEntity;
 import com.aim.foodtaxi.dto.Bid;
+import com.aim.foodtaxi.mappers.BidMapper;
 import com.aim.foodtaxi.repositories.BidRepository;
-import com.aim.foodtaxi.repositories.OrderRepository;
+import com.aim.foodtaxi.repositories.DeliveryRepository;
+import com.aim.foodtaxi.repositories.DriverRepository;
 
 @Service
 @Transactional
 public class BidService {
+	
     @Autowired
     private BidRepository bidRepository;
     
     @Autowired
-    private DeliveryRepository orderRepository;
+    private DeliveryRepository deliveryRepository;
+    
+    @Autowired
+    private DriverRepository driverRepository;
+    
+    @Autowired
+    private BidMapper bidMapper;
 
-//    @Inject
-//    private DriverRepository driverRepository;
-//    
-//    @Inject
-//    private BidMapper bidMapper;
-//
-    public boolean createBid(Bid bid, Long orderId, String driverUsername) {
+    public boolean createBid(Bid bid, Long deliveryId, String driverUsername) {
         BidEntity bidEntity = bidMapper.bidToBidEntity(bid);
-        Optional<OrderEntity> orderEntity = orderRepository.findOneById(orderId);
-        Optional<DriverEntity> driverEntity = driverRepository.findOneById(driverId);
-        if (!driverEntity.isPresent() || !orderEntity.isPresent()) {
+        Optional<DeliveryEntity> deliveryEntity = deliveryRepository.findOneById(deliveryId);
+        Optional<DriverEntity> driverEntity = driverRepository.findOneByUsername(driverUsername);
+        if (!driverEntity.isPresent() || !deliveryEntity.isPresent()) {
             return false;
         }
+        
         bidEntity.setDriver(driverEntity.get());
-        bidEntity.setOrder(orderEntity.get());
+        bidEntity.setDelivery(deliveryEntity.get());
 
 
         BidEntity savedEntity = bidRepository.save(bidEntity);
-        if (orderEntity.get().getBestBid() == null || 
-                orderEntity.get().getBestBid().getPrice() > bidEntity.getPrice()) {
-            orderEntity.get().setBestBid(savedEntity);
-            orderRepository.save(orderEntity.get());
+        if (deliveryEntity.get().getBestBid() == null || 
+                deliveryEntity.get().getBestBid().getPrice() > bidEntity.getPrice()) {
+            deliveryEntity.get().setBestBid(savedEntity);
+            deliveryRepository.save(deliveryEntity.get());
         }
         return true;
     }
