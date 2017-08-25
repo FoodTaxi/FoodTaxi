b892cc26060658ec73bc6457a33de13159931a64
5koff <dim.petkoff@gmail.com>
2017-08-25 14:12:06 +0300

    implemented "get" service for orders to be confirmed by shop

--- source/backend/FoodTaxi/src/main/java/com/aim/foodtaxi/services/OrderService.java
+++ source/backend/FoodTaxi/src/main/java/com/aim/foodtaxi/services/OrderService.java
@@ -1,6 +1,8 @@
 package com.aim.foodtaxi.services;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 
@@ -16,124 +18,125 @@ import com.aim.foodtaxi.domain.DeliveryEntity;
 import com.aim.foodtaxi.domain.OrderEntity;
 import com.aim.foodtaxi.domain.ShopEntity;
 import com.aim.foodtaxi.dto.CreateOrder;
+import com.aim.foodtaxi.dto.Order;
 import com.aim.foodtaxi.enums.DeliveryStatus;
 import com.aim.foodtaxi.enums.OrderStatus;
+import com.aim.foodtaxi.mappers.OrderMapper;
 import com.aim.foodtaxi.repositories.BrandRepository;
 import com.aim.foodtaxi.repositories.ClientRepository;
 import com.aim.foodtaxi.repositories.DeliveryRepository;
 import com.aim.foodtaxi.repositories.OrderRepository;
+import com.aim.foodtaxi.repositories.ShopRepository;
 
 @Service
 @Transactional
 public class OrderService {
-    
-    @Autowired
-    private OrderRepository orderRepository;
 
-//    @Autowired
-//    private OrderMapper orderMapper;
-    
-    @Autowired
-    private BrandRepository brandRepository;
-    
-    @Autowired
-    private ClientRepository clientRepository;
-    
-    @Autowired
-    private DeliveryRepository delivaryRepository;
-    
-    public void createOrder(CreateOrder order) throws EntityNotFoundException {
-        
-        Optional<ClientEntity> clientEntity = clientRepository.findOneById(order.getClientId());
-        if (!clientEntity.isPresent()) {
-            throw new EntityNotFoundException();
-        }
-        Optional<BrandEntity> brandEntity = brandRepository.findOneById(order.getBrandId());
-        if (!brandEntity.isPresent()) {
-            throw new EntityNotFoundException();
-        }
-        OrderEntity orderEntity = generateOrder(order, clientEntity.get(), brandEntity.get());
-        orderEntity = orderRepository.save(orderEntity);
-        DeliveryEntity deliveryEntity = generateDelivery(order, orderEntity);
-        deliveryEntity = delivaryRepository.save(deliveryEntity);
-    }
-    
-    private OrderEntity generateOrder (CreateOrder order, ClientEntity clientEntity, BrandEntity brandEntity) {
-        OrderEntity orderEntity = new OrderEntity();
-        orderEntity.setItemDescription(order.getItemDescription());
-        orderEntity.setClient(clientEntity);
-        orderEntity.setBrand(brandEntity);
-        orderEntity.setOrderDate(new Date());
-        orderEntity.setStatus(OrderStatus.AWAITING_CONFIRMATION);
-        orderEntity.setOrderValue(order.getOrderValue());
-        orderEntity.setShop(findBestShop(order, brandEntity));
-        return orderEntity;
-    }
-    private DeliveryEntity generateDelivery(CreateOrder order, OrderEntity orderEntity) {
-        DeliveryEntity delivery = new DeliveryEntity();
-        delivery.setCodAmount(order.getCodAmount());
-        delivery.setDescription(order.getItemDescription());
-        delivery.setDueDate(order.getDueDate());
-        delivery.setEndAddressText(order.getAddress());
-        delivery.setEndLatitude(order.getLatitude());
-        delivery.setEndLongtitude(order.getLongtitude());
-        delivery.setHasCod(order.isHasCod());
-        delivery.setOrder(orderEntity);
-        delivery.setOrderValue(order.getOrderValue());
-        delivery.setPin(generatePin());
-        delivery.setStartAddressText(orderEntity.getShop().getAddressText());
-        delivery.setStartLatitude(orderEntity.getShop().getLatitude());
-        delivery.setStartLongtitude(orderEntity.getShop().getLongtitude());
-        delivery.setStatus(DeliveryStatus.NEW);
-        return delivery;
-    }
-    
-    private String generatePin() {
-        int minimum = 1000;
-        Random randomGenerator = new Random();
-        int nextInt = randomGenerator.nextInt(8999);
-        return String.valueOf(minimum + nextInt);
-    }
+	@Autowired
+	private OrderRepository orderRepository;
 
-    // This method returns the shop which is closest to the order per brand
-    private ShopEntity findBestShop(CreateOrder order, BrandEntity brand) {
-        ShopEntity bestShop = null;
-        Double currentBestDistance = null;
-        for (ShopEntity shop : brand.getShops()) {
-            Double latitude = order.getLatitude() - shop.getLatitude();
-            latitude = latitude * latitude;
-            Double longtitude = order.getLongtitude() - shop.getLongtitude();
-            longtitude = longtitude * longtitude;
 
-            if (bestShop == null || currentBestDistance > longtitude + latitude) {
-                bestShop = shop;
-                currentBestDistance = longtitude + latitude;
-            }
-        }
-        return bestShop;
-    }
+	@Autowired
+	private BrandRepository brandRepository;
 
-//    public List<Order> getOrdersWithoutDrivers() {
-//        List<OrderEntity> orders = orderRepository.findAllByDriverIsNull();
-//        return orderMapper.orderEntitiesToOrders(orders);
-//    }
-//
-//    public HttpStatus createUnknownOrder(UnknownOrder order) {
-//        Optional<ShopEntity> fakeShop = shopRepository.findFirstByBrandIdAndLatitudeAndLongtitude((long) 0, order.getFromLatitude(), order.getFromLongtitude());
-//        ShopEntity shopEntity = null;
-//        if (!fakeShop.isPresent()) {
-//           shopEntity = new ShopEntity();
-//           shopEntity.setAddressText(order.getFromAddressText());
-//           shopEntity.setLatitude(order.getFromLatitude());
-//           shopEntity.setLongtitude(order.getFromLongtitude());
-//           shopEntity.setName("Unknown");
-//           shopRepository.save(shopEntity);
-//        } else {
-//            shopEntity = fakeShop.get();
-//        }
-//        OrderEntity orderEntity = orderMapper.unknownOrderToOrderEntity(order);
-//        orderEntity.setShop(shopEntity);
-//        orderRepository.save(orderEntity);
-//        return HttpStatus.CREATED;
-//    }
+	@Autowired
+	private ClientRepository clientRepository;
+
+	@Autowired
+	private DeliveryRepository delivaryRepository;
+
+	@Autowired
+	private ShopRepository shopRepository;
+
+	@Autowired
+	private OrderMapper orderMapper;
+
+	public void createOrder(CreateOrder order) throws EntityNotFoundException {
+
+		Optional<ClientEntity> clientEntity = clientRepository.findOneById(order.getClientId());
+		if (!clientEntity.isPresent()) {
+			throw new EntityNotFoundException();
+		}
+		Optional<BrandEntity> brandEntity = brandRepository.findOneById(order.getBrandId());
+		if (!brandEntity.isPresent()) {
+			throw new EntityNotFoundException();
+		}
+		OrderEntity orderEntity = generateOrder(order, clientEntity.get(), brandEntity.get());
+		orderEntity = orderRepository.save(orderEntity);
+		DeliveryEntity deliveryEntity = generateDelivery(order, orderEntity);
+		deliveryEntity = delivaryRepository.save(deliveryEntity);
+	}
+
+	private OrderEntity generateOrder(CreateOrder order, ClientEntity clientEntity, BrandEntity brandEntity) {
+		OrderEntity orderEntity = new OrderEntity();
+		orderEntity.setItemDescription(order.getItemDescription());
+		orderEntity.setClient(clientEntity);
+		orderEntity.setBrand(brandEntity);
+		orderEntity.setOrderDate(new Date());
+		orderEntity.setStatus(OrderStatus.AWAITING_CONFIRMATION);
+		orderEntity.setOrderValue(order.getOrderValue());
+		orderEntity.setShop(findBestShop(order, brandEntity));
+		return orderEntity;
+	}
+
+	private DeliveryEntity generateDelivery(CreateOrder order, OrderEntity orderEntity) {
+		DeliveryEntity delivery = new DeliveryEntity();
+		delivery.setCodAmount(order.getCodAmount());
+		delivery.setDescription(order.getItemDescription());
+		delivery.setDueDate(order.getDueDate());
+		delivery.setEndAddressText(order.getAddress());
+		delivery.setEndLatitude(order.getLatitude());
+		delivery.setEndLongtitude(order.getLongtitude());
+		delivery.setHasCod(order.isHasCod());
+		delivery.setOrder(orderEntity);
+		delivery.setOrderValue(order.getOrderValue());
+		delivery.setPin(generatePin());
+		delivery.setStartAddressText(orderEntity.getShop().getAddressText());
+		delivery.setStartLatitude(orderEntity.getShop().getLatitude());
+		delivery.setStartLongtitude(orderEntity.getShop().getLongtitude());
+		delivery.setStatus(DeliveryStatus.NEW);
+		return delivery;
+	}
+
+	private String generatePin() {
+		int minimum = 1000;
+		Random randomGenerator = new Random();
+		int nextInt = randomGenerator.nextInt(8999);
+		return String.valueOf(minimum + nextInt);
+	}
+
+	// This method returns the shop which is closest to the order per brand
+	private ShopEntity findBestShop(CreateOrder order, BrandEntity brand) {
+		ShopEntity bestShop = null;
+		Double currentBestDistance = null;
+		for (ShopEntity shop : brand.getShops()) {
+			Double latitude = order.getLatitude() - shop.getLatitude();
+			latitude = latitude * latitude;
+			Double longtitude = order.getLongtitude() - shop.getLongtitude();
+			longtitude = longtitude * longtitude;
+
+			if (bestShop == null || currentBestDistance > longtitude + latitude) {
+				bestShop = shop;
+				currentBestDistance = longtitude + latitude;
+			}
+		}
+		return bestShop;
+	}
+
+	public List<Order> getOrdersForConfirmation(long shopId) {
+
+		ShopEntity shop = shopRepository.findOne(shopId);
+		if (shop == null) {
+			throw new RuntimeException("No shop found for id: " + shopId);
+		}
+		List<OrderEntity> orders = orderRepository.getAllByStatusAndShop(OrderStatus.AWAITING_CONFIRMATION, shop);
+		List<Order> resp = new ArrayList<>();
+		
+		if (orders != null && !orders.isEmpty()) {
+			resp = orderMapper.orderEntitiesToOrders(orders);
+		}
+
+		return resp;
+
+	}
 }
