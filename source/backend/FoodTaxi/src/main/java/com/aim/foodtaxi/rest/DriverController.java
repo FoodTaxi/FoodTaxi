90ea22806c51a2f7749b987231a934bc5ebf6a86
dimpetkoff <dim.petkoff@gmail.com>
2017-12-19 17:52:19 +0200

    init commit

--- source/backend/FoodTaxi/src/main/java/com/aim/foodtaxi/rest/DriverController.java
+++ source/backend/FoodTaxi/src/main/java/com/aim/foodtaxi/rest/DriverController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,23 +22,28 @@ import springfox.documentation.annotations.ApiIgnore;
 @RequestMapping("/api/private/driver")
 public class DriverController {
 
-    @Autowired
-    private DriverService driverService;
+	@Autowired
+	private DriverService driverService;
 
-	
+	@RequestMapping(value = "/me", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public ResponseEntity<?> getDriver(@RequestHeader(value = "authorization") String authString,
+			@ApiIgnore Principal principal) {
+		String username = principal.getName();
+		if (username == null) {
+			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+		}
+		Driver driver = driverService.getDriverByUsername(username);
+		if (driver != null) {
+			return new ResponseEntity<Driver>(driver, HttpStatus.OK);
+		}
+		return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+	}
 
-    @RequestMapping(value = "/me", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public ResponseEntity<?> getDriver(@RequestHeader(value = "authorization") String authString,
-             @ApiIgnore Principal principal) {
-        String username = principal.getName();
-        if (username == null) {
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-        }
-        Driver driver = driverService.getDriverByUsername(username);
-        if (driver != null) {
-            return new ResponseEntity<Driver>(driver, HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-    }
+	public ResponseEntity<?> updateLocation(@RequestHeader(value = "authorization") String authString,
+			@ApiIgnore Principal principal, @RequestParam(name = "lat", required = true) double lat,
+			@RequestParam(name = "lng", required = true) double lng) {
+
+		return null;
+	}
 }
